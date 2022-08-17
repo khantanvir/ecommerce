447666a49dba7f7d2b30a06ff4ecfda8538c07f3
Tanvir <aiub.tanvir@zohomail.com>
2022-08-17 00:36:43 -0700

    seeder only

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(RoleSeeder::class);
-        //$this->call(AdminUserSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(AdminUserSeeder::class);
         $this->call(AttributeSeeder::class);
     }
 }
